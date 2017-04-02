08191517a4d2e4869e1e8e66c7385fd2d61f8154
ryanosaur <ryan.justin.taylor@gmail.com>
2017-04-01 22:28:33 -0400

    header styling init

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,17 @@ class Header extends React.Component {
 
   render() {
     return (
-      <div className="sticky-header navigation-header">
+      <div className="sticky-header">
         <div className="brand-name-wrap">
-          <span className="brand-name-text">&lt;Ryan Taylor developer=&#123;true&#125; /&gt;</span>
+          <span className="brand-name-text">
+            <span className="react-brackets">&lt;</span>
+            <span className="react-element">RyanTaylor</span>
+            <span className="react-attr">developer=
+              <span className="react-brackets">&#123;</span>
+              <span className="react-value">true</span>
+              <span className="react-brackets">&#125;</span></span>
+            <span className="react-brackets">/&gt;</span>
+          </span>
         </div>
       </div>
     );
