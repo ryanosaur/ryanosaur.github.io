e276a2fc62caa1ac322d3f8394b2cb9a66d60eaf
ryanosaur <ryan.justin.taylor@gmail.com>
2017-04-04 00:40:35 -0400

    more content

--- src/components/Home.js
+++ src/components/Home.js
@@ -35,8 +35,8 @@ class Home extends React.Component {
           <div className="home-header-details-wrap">
             <h1 className="home-header-detail-item home-header-name">Ryan Taylor</h1>
             <h3 className="home-header-detail-item home-header-title">Software Developer</h3>
-            <h3 className="home-header-detail-item home-header-email clickable"><a href="mailto:ryan.justin.taylor@gmail.com">ryan.justin.taylor@gmail.com</a></h3>
-            <h3 className="home-header-detail-item home-header-telephone clickable"><a href="tel:+14089148921">+1 (408) 914-8921</a></h3>
+            <h3 className="home-header-detail-item home-header-email"><a className="external-link" href="mailto:ryan.justin.taylor@gmail.com">ryan.justin.taylor@gmail.com</a></h3>
+            <h3 className="home-header-detail-item home-header-telephone"><a className="external-link" href="tel:+14089148921">+1 (408) 914-8921</a></h3>
           </div>
         </section>
         <section className="home-header-section">
@@ -44,7 +44,32 @@ class Home extends React.Component {
             <h2 className="home-section-title">Who am I?</h2>
           </div>
           <div className="home-section-content-wrap">
-            <p className="home-section-content">This is a paragraph.</p>
+            <p className="home-section-content">
+              Oh boy, we're starting with existentialism? Well, first and foremost I'm a Newfoundlander. If you've never noticed it on a map,
+              it's the (not so little) island off the coast of Canada that resembles a foam finger people wear to sports games. There's about 500 000
+              of us in existence at any given time, so we're a pretty rare breed.
+            </p>
+          </div>
+          <div className="home-section-content-wrap">
+            <p className="home-section-content">
+              More relevently, I'm a data driven software developer -- with a love for VIM. After college, I started working at startup in Newfoundland
+              called <a href="https://verafin.com/" className="external-link" target="_blank">Verafin</a> doing first integrations between their software and core banking systems
+              in the United States and Canada, and later between their software and <a href="https://www.salesforce.com/" className="external-link" target="_blank">Salesforce.com</a>.
+            </p>
+          </div>
+          <div className="home-section-content-wrap">
+            <p className="home-section-content">
+              In the spring of 2015, I decided I was ready for a new challenge. One that would marry my creative mind with my technical talents. So I
+              travelled to Fremont, California to join a 90 day javascript intensive bootcamp. There I would learn all of the wizardry of modern front-end
+              development.
+            </p>
+          </div>
+          <div className="home-section-content-wrap">
+            <p className="home-section-content">
+              While in California job searching, I was approached by <a href="http://canvs.tv/" className="external-link" target="_blank">Canvs</a>. Canvs has given me the opportunity
+              to move to New York City and lead a team of developers to creating their new front-end application in whatever libraries and architecture I chose.
+              For that, I will be forever grateful.
+            </p>
           </div>
         </section>
         <section className="home-header-section">
